f362e9bbb26d406bd00bb0efbad2828244291e55
Jeriel Martinez <martinez.jeriel01@gmail.com>
2024-02-20 14:46:59 -0500

    store

--- src/index.js
+++ src/index.js
@@ -34,17 +34,15 @@ let shopItemsData = [{
     price: 5.99,
     desc: "Support your cat's need to maintain a healthy weight with Purina ONE Ideal Weight High Protein Natural With Added Vitamins, Minerals and Nutrients dry cat food. (...)",
     img: "src/imgs/cat-food/cat-food-5.png"
-}]
+}];
 
-let basket = [{
-    id: "",
-    item: 1
-}]
+let basket = JSON.parse(localStorage.getItem("basketData")) || []
 
 let generateShop = () => {
     
     return (shop.innerHTML = shopItemsData.map((x)=>{
         let { id, name, price, desc, img } = x;
+        let search = basket.find((x)=>x.id === id) || [];
         return  `
         <div id=product-id-${id} class="shop-item">
             <span class="shop-item-title">${name}</span>
@@ -61,11 +59,13 @@ let generateShop = () => {
                 </div>
                     <span class="shop-item-price">${price}</span>
                     <div class="shop-item-details">
-                        <button onclick="increment(${id})" id=${id} class="shop-item-button" type="button">ADD TO CART
+                        <button class="shop-item-button" type="button">ADD TO CART
                         </button>
-                    <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                    <div id=${id} class="quantity">0</div>
-                    <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                    <i onclick="decrement('${id}')" class="bi bi-dash-lg"></i>
+                    <div id=${id} class="quantity">
+                    ${search.item === undefined? 0: search.item}
+                    </div>
+                    <i onclick="increment('${id}')" class="bi bi-plus-lg"></i>
                 </div>
         </div>`
     }).join(""));
@@ -74,8 +74,49 @@ let generateShop = () => {
 generateShop();
 
 let increment = (id) => {
-    let selectedItem = id;
-    console.log(increment);
+    let search = basket.find((basketItem) => basketItem.id === id);
+
+    if (search === undefined) {
+        basket.push({
+            id: id,
+            item: 1,
+        });
+    } else {
+        search.item += 1;
+    }
+    localStorage.setItem("basketData", JSON.stringify(basket));
+    update(id);
 };
-let decrement = () => {};
-let update = () => {};
+
+let decrement = (id) => {
+    let search = basket.find((basketItem) => basketItem.id === id);
+    
+    if (search.item === 0) return
+    else if (search === undefined || search.item === 0) return;
+    else {
+        search.item -= 1;
+    }
+    
+    update(id);
+
+    basket = basket.filter((basketFilter)=> basketFilter.item !== 0);
+
+    localStorage.setItem("basketData", JSON.stringify(basket));
+};
+
+
+let update = (id) => {
+    let search = basket.find((basketItem)=> basketItem.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calculation()
+};
+
+let calculation = () => {
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket
+    .map((basketItem) => parseInt(basketItem.item))
+    .reduce((x, y) => x + y, 0);
+
+};
+
+calculation();
